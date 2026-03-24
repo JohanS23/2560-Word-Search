d898af8d019bb95ce054be30f5828cee30e60b80
dgklingsporn <dgklingsporn@gmail.com>
2026-03-24 11:59:17 -0400

    Create grid.h

---
+++ grid.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include "matrix.h"
+
+using namespace std;
+
+class grid {
+public:
+    matrix<char> m;
+
+    grid(string file_name, int r, int c) : m(r, c, 0) {
+        ifstream inputFile(file_name);
+        if (!inputFile)
+            cout << "Error: could not open file" << endl;
+
+        for (int i = 0; i < m.rows(); i++)
+            for (int j = 0; j < m.cols(); j++)
+                inputFile >> m[i][j];
+    }
+};
