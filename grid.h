4c50a9da0de819868e812b339afa8c71da73a40f
dgklingsporn <dgklingsporn@gmail.com>
2026-03-24 22:56:30 -0400

    Update grid.h
    comments

--- grid.h
+++ grid.h
@@ -6,16 +6,16 @@ using namespace std;
 
 class grid {
 public:
-    matrix<char> m;
+    matrix<char> m; // create the maxtrix
 
-    grid(string file_name, int r, int c) : m(r, c, 0) {
+    grid(string file_name, int r, int c) : m(r, c, 0) { // pass the file name, rows and collums into the constructor
         ifstream inputFile(file_name);
-        if (!inputFile)
+        if (!inputFile) // file error handling
             cout << "Error: could not open file" << endl;
         int rows, cols;
         inputFile >> rows >> cols;  // skip the first line (dimensions)
 
-        for (int i = 0; i < m.rows(); i++)
+        for (int i = 0; i < m.rows(); i++) // iterate through the matrix and add the corrisponding element to the matrix
             for (int j = 0; j < m.cols(); j++)
                 inputFile >> m[i][j];
     }
